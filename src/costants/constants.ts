aa699a079692e603b7dd54f6a17f88266a362c2e
RubenPari <rubenpari.developer@gmail.com>
2024-10-15 16:38:14 +0200

    added constants file

---
+++ src/costants/constants.ts
@@ -0,0 +1,21 @@
+const INTERNATIONAL_RADIO_EDIT = "International Radio Edit";
+const RADIO_EDIT = "Radio Edit";
+const LIVE = "Live";
+const EDITED = "(Edited)";
+const ACOUSTIC = "Acoustic";
+const INSTRUMENTAL = "Instrumental";
+const NBA_VERSION = "NBA Version";
+const DETRIMENTAL = "Detrimental";
+const CLEAR = "Clear";
+
+export {
+  ACOUSTIC,
+  CLEAR,
+  DETRIMENTAL,
+  EDITED,
+  INSTRUMENTAL,
+  INTERNATIONAL_RADIO_EDIT,
+  LIVE,
+  NBA_VERSION,
+  RADIO_EDIT,
+};
