aeee6c9d2345dcfdb693c4feda114c020f31d32e
RubenPari <rubenpari.developer@gmail.com>
2024-10-15 16:40:36 +0200

    mapped all routes for application

---
+++ src/routes/routes.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from "fastify";
+import { callback, login, logout } from "../controllers/authControllers.ts";
+import featArtist from "../controllers/featArtistController.ts";
+
+export default (app: FastifyInstance) => {
+    app.register((auth, _opts, done) => {
+        auth.get("/login", login);
+        auth.get("/callback", callback);
+        auth.get("/logout", logout);
+        done();
+    }, { prefix: "/auth" });
+
+    app.register((base, _opts, done) => {
+        base.post("/feat-artist", featArtist);
+        done();
+    });
+};
